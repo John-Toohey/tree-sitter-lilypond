bf3122141c4db276644978de1e46d91cb62bb1bd
John Toohey <john_t@mailo.com>
2025-05-06 19:33:16 +0100

    Initial work on making it helix compatible

---
+++ shell.nix
@@ -0,0 +1,5 @@
+let pkgs = import <nixpkgs> { };
+in pkgs.mkShell {
+  packages =
+    [ (pkgs.python3.withPackages (python-pkgs: [ python-pkgs.pygments ])) ];
+}
--- update_builtins.py
+++ update_builtins.py
@@ -5,35 +5,39 @@ import re
 from textwrap import dedent
 
 # \include and \version are actually handled by LilyPond’s lexer as a preprocessing step.
-for keyword in ['include', 'version']:
+for keyword in ["include", "version"]:
     _lilypond_builtins.keywords.remove(keyword)
 # \inherit-acceptability and \language are actually music functions.
-for keyword in ['inherit-acceptability', 'language']:
+for keyword in ["inherit-acceptability", "language"]:
     _lilypond_builtins.keywords.remove(keyword)
 # Add keywords that are not treated as such in Pygments.
 keywords = [
-    'alternative',
-    'change',
-    'override',
-    'repeat',
-    'revert',
-    'set',
-    'tempo',
-    'unset',
+    "alternative",
+    "change",
+    "override",
+    "repeat",
+    "revert",
+    "set",
+    "tempo",
+    "unset",
 ]
 _lilypond_builtins.keywords.extend(keywords)
-_lilypond_builtins.keywords.extend([
-    'default',
-    'lyrics',
-    'markup',
-    'markuplist',
-    'rest',
-    'sequential',
-    'simultaneous',
-])
+_lilypond_builtins.keywords.extend(
+    [
+        "default",
+        "lyrics",
+        "markup",
+        "markuplist",
+        "rest",
+        "sequential",
+        "simultaneous",
+    ]
+)
 
 # Remove clefs that must be expressed as strings.
-_lilypond_builtins.clefs = [clef for clef in _lilypond_builtins.clefs if not re.search(r'\d', clef)]
+_lilypond_builtins.clefs = [
+    clef for clef in _lilypond_builtins.clefs if not re.search(r"\d", clef)
+]
 
 # Remove keywords from music functions.
 for keyword in keywords:
@@ -44,98 +48,108 @@ music_objects = set(_lilypond_builtins.music_commands)
 music_objects.update(_lilypond_builtins.articulations)
 music_objects.update(_lilypond_builtins.dynamics)
 # Remove punctuation marks.
-for item in ['!', '(', ')', '-', '<', '>', '[', ']', '^', '|', '~']:
+for item in ["!", "(", ")", "-", "<", ">", "[", "]", "^", "|", "~"]:
     music_objects.remove(item)
 
 # Remove keywords from markup commands.
-for keyword in ['markup', 'markuplist', 'override', 'score']:
+for keyword in ["markup", "markuplist", "override", "score"]:
     _lilypond_builtins.markup_commands.remove(keyword)
 
-with open('queries/highlights-builtins.scm', 'w') as file:
-    backslash_prefix = r'^\\\\'
+with open("queries/highlights-builtins.scm", "w") as file:
+    backslash_prefix = r"^\\\\"
     for list_and_selector in [
-        (_lilypond_builtins.keywords, 'keyword'),
-        (_lilypond_builtins.music_functions, 'identifier.core.function'),
-        (music_objects, 'identifier.core.global'),
-        (_lilypond_builtins.markup_commands, 'identifier.core.function.markup'),
+        (_lilypond_builtins.keywords, "keyword"),
+        (_lilypond_builtins.music_functions, "function"),
+        (music_objects, "constants.builtin"),
+        (_lilypond_builtins.markup_commands, "function.markup"),
     ]:
-        file.write(dedent(f'''\
+        file.write(
+            dedent(
+                f'''\
         (
           (escaped_word) @{list_and_selector[1]}
           (#match? @{list_and_selector[1]} "{regex_opt(list_and_selector[0], backslash_prefix, '$').replace('\\-', '-')}")
         )
 
-        '''))
+        '''
+            )
+        )
 
     for list_and_selector in [
-        (_lilypond_builtins.grobs, 'identifier.core.type.graphical_object'),
-        (_lilypond_builtins.contexts, 'identifier.core.type.context'),
-        (_lilypond_builtins.translators, 'identifier.core.type.translator'),
-        (_lilypond_builtins.context_properties, 'identifier.core.property.context'),
+        (_lilypond_builtins.grobs, "constant"),
+        (_lilypond_builtins.contexts, "constant"),
+        (_lilypond_builtins.translators, "constant"),
+        (_lilypond_builtins.context_properties, "constant"),
     ]:
-        file.write(dedent(f'''\
+        file.write(
+            dedent(
+                f"""\
         (
           (symbol) @{list_and_selector[1]}
           (#match? @{list_and_selector[1]} "{regex_opt(list_and_selector[0], '^', '$')}")
         )
 
-        '''))
+        """
+            )
+        )
 
-    file.write(dedent(f'''\
+    file.write(
+        dedent(
+            f"""\
     (
       (
-        (escaped_word) @identifier.core.function
-        (#match? @identifier.core.function "^\\\\\\\\tweak$")
+        (escaped_word) @function
+        (#match? @function "^\\\\\\\\tweak$")
       )
       .
       (
-        (symbol) @identifier.core.property.graphical_object
-        (#match? @identifier.core.property.graphical_object "{regex_opt(_lilypond_builtins.grob_properties, '^', '$')}")
+        (symbol) @label
+        (#match? @label "{regex_opt(_lilypond_builtins.grob_properties, '^', '$')}")
       )
     )
 
     (property_expression
       (
-        (symbol) @identifier.core.property.graphical_object
-        (#match? @identifier.core.property.graphical_object "{regex_opt(_lilypond_builtins.grob_properties, '^', '$')}")
+        (symbol) @label 
+        (#match? @label "{regex_opt(_lilypond_builtins.grob_properties, '^', '$')}")
       )
     )
 
     (
       (
-        (escaped_word) @identifier.core.function
-        (#match? @identifier.core.function "^\\\\\\\\clef$")
+        (escaped_word) @function
+        (#match? @function "^\\\\\\\\clef$")
       )
       .
       (
-        (symbol) @identifier.core.constant.clef
-        (#match? @identifier.core.constant.clef "{regex_opt(_lilypond_builtins.clefs, '^', '$')}")
+        (symbol) @constant.clef
+        (#match? @constant.clef "{regex_opt(_lilypond_builtins.clefs, '^', '$')}")
       )
     )
 
     (
       (
-        (escaped_word) @identifier.core.function
-        (#match? @identifier.core.function "^\\\\\\\\key$")
+        (escaped_word) @function
+        (#match? @function "^\\\\\\\\key$")
       )
       .
       (symbol)
       .
       (
-        (escaped_word) @identifier.core.constant.scale
-        (#match? @identifier.core.constant.scale "{regex_opt(_lilypond_builtins.scales, backslash_prefix, '$')}")
+        (escaped_word) @constant
+        (#match? @constant  "{regex_opt(_lilypond_builtins.scales, backslash_prefix, '$')}")
       )
     )
 
     (
       (
-        (escaped_word) @identifier.core.function
-        (#match? @identifier.core.function "^\\\\\\\\repeat$")
+        (escaped_word) @function
+        (#match? @function "^\\\\\\\\repeat$")
       )
       .
       (
-        (symbol) @identifier.core.constant.repeat_type
-        (#match? @identifier.core.constant.repeat_type "{regex_opt(_lilypond_builtins.repeat_types, '^', '$')}")
+        (symbol) @constant
+        (#match? @constant "{regex_opt(_lilypond_builtins.repeat_types, '^', '$')}")
       )
     )
 
@@ -147,8 +161,8 @@ with open('queries/highlights-builtins.scm', 'w') as file:
       .
       (expression_block
         (
-          (escaped_word) @identifier.core.constant.unit
-          (#match? @identifier.core.constant.unit "{regex_opt(_lilypond_builtins.units, backslash_prefix, '$')}")
+          (escaped_word) @iconstant
+          (#match? @constant "{regex_opt(_lilypond_builtins.units, backslash_prefix, '$')}")
         )
       )
     )
@@ -169,13 +183,13 @@ with open('queries/highlights-builtins.scm', 'w') as file:
 
     (
       (
-        (escaped_word) @identifier.core.function
-        (#match? @identifier.core.function "^\\\\\\\\language$")
+        (escaped_word) @function
+        (#match? @function "^\\\\\\\\language$")
       )
       .
       (
-        (symbol) @identifier.core.constant.language
-        (#match? @identifier.core.constant.language "{regex_opt(_lilypond_builtins.pitch_language_names, '^', '$')}")
+        (symbol) @constant
+        (#match? @constant "{regex_opt(_lilypond_builtins.pitch_language_names, '^', '$')}")
       )
     )
 
@@ -189,14 +203,14 @@ with open('queries/highlights-builtins.scm', 'w') as file:
         (assignment_lhs
           [
             (
-              (symbol) @identifier.core.variable
-              (#match? @identifier.core.variable "{regex_opt(_lilypond_builtins.paper_variables, '^', '$')}")
+              (symbol) @variable
+              (#match? @variable "{regex_opt(_lilypond_builtins.paper_variables, '^', '$')}")
             )
 
             (property_expression
               (
-                (symbol) @identifier.core.variable
-                (#match? @identifier.core.variable "{regex_opt(_lilypond_builtins.paper_variables, '^', '$')}")
+                (symbol) @variable
+                (#match? @variable "{regex_opt(_lilypond_builtins.paper_variables, '^', '$')}")
               )
             )
           ]
@@ -212,8 +226,8 @@ with open('queries/highlights-builtins.scm', 'w') as file:
       .
       (expression_block
         (
-          (escaped_word) @identifier.core.variable
-          (#match? @identifier.core.variable "{regex_opt(_lilypond_builtins.paper_variables, backslash_prefix, '$')}")
+          (escaped_word) @variable
+          (#match? @variable "{regex_opt(_lilypond_builtins.paper_variables, backslash_prefix, '$')}")
         )
       )
     )
@@ -226,38 +240,64 @@ with open('queries/highlights-builtins.scm', 'w') as file:
       .
       (expression_block
         (assignment_lhs
-          (symbol) @identifier.core.variable
-          (#match? @identifier.core.variable "{regex_opt(_lilypond_builtins.header_variables, '^', '$')}")
+          (symbol) @variable
+          (#match? @variable "{regex_opt(_lilypond_builtins.header_variables, '^', '$')}")
         )
       )
     )
-    '''))
+    """
+        )
+    )
 
-with open('queries/highlights-scheme-builtins.scm', 'w') as file:
-    regex = re.sub(r'\\(.)', r'\\\\\1', regex_opt(_scheme_builtins.scheme_keywords, '^', '$').replace('\\-', '-'))
-    file.write(dedent(f'''\
+with open("queries/highlights-scheme-builtins.scm", "w") as file:
+    regex = re.sub(
+        r"\\(.)",
+        r"\\\\\1",
+        regex_opt(_scheme_builtins.scheme_keywords, "^", "$").replace("\\-", "-"),
+    )
+    file.write(
+        dedent(
+            f"""\
     (
       (scheme_symbol) @keyword
       (#match? @keyword "{regex}")
     )
-    '''))
+    """
+        )
+    )
 
     # Remove operator-like functions.
-    for item in ['*', '+', '-', '/', '<', '<=', '=', '>', '>=']:
+    for item in ["*", "+", "-", "/", "<", "<=", "=", ">", ">="]:
         _scheme_builtins.scheme_builtins.remove(item)
-    regex = re.sub(r'\\(.)', r'\\\\\1', regex_opt(_scheme_builtins.scheme_builtins, '^', '$').replace('\\-', '-'))
-    file.write(dedent(f'''\
-    (
-      (scheme_symbol) @identifier.core.function
-      (#match? @identifier.core.function "{regex}")
+    regex = re.sub(
+        r"\\(.)",
+        r"\\\\\1",
+        regex_opt(_scheme_builtins.scheme_builtins, "^", "$").replace("\\-", "-"),
     )
-    '''))
+    file.write(
+        dedent(
+            f"""\
+    (
+      (scheme_symbol) @function
+      (#match? @function "{regex}")
+    )
+    """
+        )
+    )
 
-with open('queries/highlights-lilypond-builtins.scm', 'w') as file:
-    regex = re.sub(r'\\(.)', r'\\\\\1', regex_opt(_lilypond_builtins.scheme_functions, '^', '$').replace('\\-', '-'))
-    file.write(dedent(f'''\
-    (
-      (scheme_symbol) @identifier.core.function
-      (#match? @identifier.core.function "{regex}")
+with open("queries/highlights-lilypond-builtins.scm", "w") as file:
+    regex = re.sub(
+        r"\\(.)",
+        r"\\\\\1",
+        regex_opt(_lilypond_builtins.scheme_functions, "^", "$").replace("\\-", "-"),
     )
-    '''))
+    file.write(
+        dedent(
+            f"""\
+    (
+      (scheme_symbol) @function
+      (#match? @function "{regex}")
+    )
+    """
+        )
+    )
